320a9bb958b0c27817ec65d283028d8a79fad2f8
pei <pei@anvei.com>
2025-06-13 14:41:15 +0800

    rounded corner image

--- src/main.rs
+++ src/main.rs
@@ -60,21 +60,35 @@ async fn main() -> anyhow::Result<()> {
     }
 
     {
-        let defAvatar = slint::Image::load_from_path(std::path::Path::new("ui/assets/images/img.jpg")).unwrap();
+        let def_avatar = slint::Image::load_from_path(std::path::Path::new("ui/assets/images/img.jpg")).unwrap();
         //test list
         let model = slint::VecModel::<MyListViewItem>::from(vec![
-            MyListViewItem { id: 2i64, from_type: 1,  text: slint::SharedString::from("Item 1"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 3i64, from_type: 1, text: slint::SharedString::from("Item 2"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 5i64, from_type: 1, text: slint::SharedString::from("Item 3"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 7i64, from_type: 1, text: slint::SharedString::from("Item 4"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 11i64, from_type: 2, text: slint::SharedString::from("Item 5"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 13i64, from_type: 2, text: slint::SharedString::from("Item 6"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 17i64, from_type: 2, text: slint::SharedString::from("Item 7"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 19i64, from_type: 2, text: slint::SharedString::from("Item 8"), avatar: defAvatar.clone() },
-            MyListViewItem { id: 23i64, from_type: 1, text: slint::SharedString::from("Item 9"), avatar: defAvatar.clone() },
+            MyListViewItem { id: 2i64, from_type: 1,  text: slint::SharedString::from("Item 1"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 3i64, from_type: 1, text: slint::SharedString::from("Item 2"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 5i64, from_type: 1, text: slint::SharedString::from("Item 3"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 7i64, from_type: 1, text: slint::SharedString::from("Item 4"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 11i64, from_type: 2, text: slint::SharedString::from("Item 5"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 13i64, from_type: 2, text: slint::SharedString::from("Item 6"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 17i64, from_type: 2, text: slint::SharedString::from("Item 7"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 19i64, from_type: 2, text: slint::SharedString::from("Item 8"), avatar: def_avatar.clone() },
+            MyListViewItem { id: 23i64, from_type: 1, text: slint::SharedString::from("Item 9"), avatar: def_avatar.clone() },
         ]);
         for i in 10..1000{
-           model.push(MyListViewItem { id: i as i64, from_type: i%2+1, text: slint::SharedString::from(format!("Hello, {}! go and find what is not just so easy to do so", i).as_str()), avatar: defAvatar.clone()});
+            match i % 4 {
+                0 => {
+                    model.push(MyListViewItem { id: i as i64, from_type: i%2+1, text: slint::SharedString::from(format!("Hello, {}! go and find what is not just so easy to do so", i).as_str()), avatar: def_avatar.clone()});
+                },
+                1 =>{
+                    model.push(MyListViewItem { id: i as i64, from_type: i%2+1, text: slint::SharedString::from(format!("{}: Text messages are used for personal, family, business, and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. In the 2010s, the sending of short informal messages became an accepted part of many cultures, as happened earlier with emailing.[5] This makes texting a quick and easy way to communicate with friends, family, and colleagues, including in contexts where a call would be impolite or inappropriate (e.g., calling very late at night or when one knows the other person is busy with family or work activities). Like e-mail and voicemail, and unlike calls (in which the caller hopes to speak directly with the recipient), texting does not require the caller and recipient to both be free at the same moment; this permits communication even between busy individuals. Text messages can also be used to interact with automated systems, for example, to order products or services from e-commerce websites or to participate in online contests. Advertisers and service providers use direct text marketing to send messages to mobile users about promotions, payment due dates, and other notifications instead of using postal mail, email, or voicemail.", i).as_str()), avatar: def_avatar.clone()});
+                },
+                2 => {
+                    model.push(MyListViewItem { id: i as i64, from_type: i%2+1, text: slint::SharedString::from(format!("{}: It has been a very influential and powerful tool in the Philippines, where in 2008 the average user sent 10–12 text messages a day. The same year, the Philippines alone sent on average over 1 billion text messages a day,[26] more than the annual average SMS volume of the countries in Europe, and even China and India. SMS saw hugely popular in India, where youngsters often exchanged many text messages, and companies provide alerts, infotainment, news, cricket scores updates, railway/airline booking, mobile billing, and banking services on SMS.[citation needed]", i).as_str()), avatar: def_avatar.clone()});
+                },
+                _=>{
+                    model.push(MyListViewItem { id: i as i64, from_type: i%2+1, text: slint::SharedString::from(format!("{}: The electrical telegraph systems, developed in the early 19th century, used electrical signals to send text messages. In the late 19th century, wireless telegraphy was developed using radio waves.", i).as_str()), avatar: def_avatar.clone()});
+                }
+            }
+           
         }
         let model = std::rc::Rc::new(model);
 
@@ -108,7 +122,7 @@ async fn main() -> anyhow::Result<()> {
 
                         let id  = if(model.row_count()<1){1} else{model.row_data(model.row_count()-1).unwrap().id+1};
                             //rng.random();
-                        let item = MyListViewItem { id: id, from_type: id as i32 %2 + 1, text: slint::SharedString::from(format!("Hello, {}!", id).as_str()), avatar: defAvatar.clone() };
+                        let item = MyListViewItem { id: id, from_type: id as i32 %2 + 1, text: slint::SharedString::from(format!("Hello, {}!", id).as_str()), avatar: def_avatar.clone() };
                         match lower_bound(&model, |val|{
                             if item.id == val.id{return  std::cmp::Ordering::Equal;}
                             if item.id < val.id{return std::cmp::Ordering::Less;}
