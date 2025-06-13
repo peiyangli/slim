db570aa24591004e9fb92c626aa71ddc3fcb1080
pei <pei@anvei.com>
2025-06-13 16:30:02 +0800

    add style build

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    slint_build::compile("ui/app_window.slint").expect("Slint build failed");
+    let config =
+        slint_build::CompilerConfiguration::new()
+        .with_style("material".into()); //material, fluent, cupertino, cosmic, native
+    slint_build::compile_with_config("ui/app_window.slint", config).expect("Slint build failed");
+    // slint_build::compile("ui/app_window.slint").expect("Slint build failed");
     embed_resource::compile("ui/assets/app.rc", embed_resource::NONE).manifest_optional().unwrap();
 }
